6a36eebe3bbdf7fd22c61ee50f9df647b3734982
TsogtOnCrack <tsogt.otgo@gmail.com>
2022-07-17 21:13:20 +0800

    begning of bug fix

--- src/pages/whatwedoing.tsx
+++ src/pages/whatwedoing.tsx
@@ -6,12 +6,18 @@ import { IgmSecondPart, OurPlanetFirst, OurPlanetSecond, IGMLAB, Incubator, Wpic
 import { useTheme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
 
+const data = {
+  dir1: 'assets/bigGrass.jpg',
+  dir: 'assets/incubatorPicture.jpg',
+  
+}
+
 const WhatWeDoing = () => {
   const theme = useTheme()
   const matches = useMediaQuery(theme.breakpoints.down('md'))
   return (
-    <MainLayout classname={`bg-backgroundColor-darkGray flex flex-col`}>
-      <div className={` max-w-shambWidth ${PADDINGX}`} id="FirstPart">
+    <MainLayout classname={`bg-backgroundColor-darkGray flex flex-col items-center ${PADDINGX} `}>
+      <div className={` flex flex-col items-start`} id="FirstPart">
         <OurPlanetFirst />
         <OurPlanetSecond />
       </div>
@@ -19,10 +25,12 @@ const WhatWeDoing = () => {
       <IgmSecondPart />
       <div id="spacer" className={matches ? 'h-20' : 'h-80'}></div>
       <IGMLAB />
+      <div id="spacer" className={matches ? 'h-20' : ' h-[280px]'}></div>
+      <Wpic dir={data.dir} />
       <div id="spacer" className={matches ? 'h-20' : 'h-[210px]'}></div>
       <Incubator />
       <div id="spacer" className={matches ? 'h-20' : 'h-[312px]'}></div>
-      <Wpic dir={'assets/bigGrass.jpg'} />
+      <Wpic dir={data.dir1} />
       <div id="spacer" className={matches ? 'h-20' : 'h-[210px]'}></div>
       <Ecard />
     </MainLayout>
